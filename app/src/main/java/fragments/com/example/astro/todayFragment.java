31405ee0e85468eeaccc45054ecd34db2e7d1ab3
Arkadiusz <arkadiusz.krupinski15@gmail.com>
2020-05-15 15:57:16 +0200

    Lepszy wyglad - telefon

--- app/src/main/java/fragments/com/example/astro/todayFragment.java
+++ app/src/main/java/fragments/com/example/astro/todayFragment.java
@@ -11,6 +11,8 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class todayFragment extends Fragment {
 
@@ -37,6 +39,19 @@ public class todayFragment extends Fragment {
     private int windSpeed;
     private int windDeg;
 
+    private TextView tName;
+    private TextView tFetchedTime;
+    private TextView tCoordLat;
+    private TextView tCoordLon;
+    private TextView tTemperature;
+    private TextView tWeatherMain;
+    private TextView tWeatherDescription;
+    private ImageView tPrettyIcon;
+    private TextView tPressure;
+    private TextView tHumidity;
+    private TextView tWindSpeed;
+    private TextView tWindDeg;
+
     public todayFragment() {
         // Required empty public constructor
     }
@@ -55,7 +70,9 @@ public class todayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today, container, false);
+        View v = inflater.inflate(R.layout.fragment_today, container, false);
+        binding(v);
+        return v;
     }
 
     @Override
@@ -63,6 +80,8 @@ public class todayFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         SharedViewModel model = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
         initialization(model);
+
+        settingTexts();
     }
 
     private void initialization(SharedViewModel model) {
@@ -88,5 +107,35 @@ public class todayFragment extends Fragment {
         weatherIcon = model.getPresent().getAsString(DatabaseHelper.WEATHER_ICON);
         windSpeed = model.getPresent().getAsInteger(DatabaseHelper.WIND_SPEED);
         windDeg = model.getPresent().getAsInteger(DatabaseHelper.WIND_DEG);
+    }
+
+    private void binding(View v) {
+        tName = v.findViewById(R.id.name);
+        tFetchedTime = v.findViewById(R.id.fetchedTime);
+        tCoordLat = v.findViewById(R.id.coordLat);
+        tCoordLon = v.findViewById(R.id.coordLon);
+        tTemperature = v.findViewById(R.id.temperature);
+        tWeatherMain = v.findViewById(R.id.weatherMain);
+        tWeatherDescription = v.findViewById(R.id.weatherDescription);
+        tPrettyIcon = v.findViewById(R.id.prettyIcon);
+        tPressure = v.findViewById(R.id.pressure);
+        tHumidity = v.findViewById(R.id.humidity);
+        tWindSpeed = v.findViewById(R.id.windSpeed);
+        tWindDeg = v.findViewById(R.id.windDeg);
+    }
+
+    private void settingTexts() {
+        tName.setText(name);
+        tFetchedTime.setText(timeText);
+        tCoordLat.setText(String.format("Lat %d", lat));
+        tCoordLon.setText(String.format("Lon %d", lon));
+        tTemperature.setText(String.valueOf(temperature));
+        tWeatherMain.setText(weatherMain);
+        tWeatherDescription.setText(weatherDescription);
+//        tPrettyIcon.setText(prett);
+        tPressure.setText(String.format("Pressure %d", pressure));
+        tHumidity.setText(String.format("Humidity %d", humidity));
+        tWindSpeed.setText(String.format("Wind speed %d", windSpeed));
+        tWindDeg.setText(String.format("Wind degree %d", windDeg));
     }
 }
--- app/src/main/java/requests/com/example/astro/RequestsManager.java
+++
@@ -1,4 +0,0 @@
-package com.example.astro;
-
-public class RequestsManager {
-}
